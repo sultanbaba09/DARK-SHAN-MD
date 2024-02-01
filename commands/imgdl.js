45d737171a7e0c75cf296b78395e827d9f0fb40e
KING-VAJIRA <129604419+vajirabot1@users.noreply.github.com>
2024-02-01 20:10:39 +0530

    Img cmd added by vajira

---
+++ commands/imgdl.js
@@ -0,0 +1,42 @@
+const moment = require('moment-timezone')
+const {fetchJson,cmd, tlang } = require('../lib')
+let gis = require("async-g-i-s");
+const axios = require('axios')
+const fetch = require('node-fetch')
+
+
+  //---------------------------------------------------------------------------
+
+cmd({
+            pattern: "img",
+            react: "📸",
+            alias :['image','imge'],
+            category: "search",
+            desc: "Searches Image on Google",
+            use: '<text>',
+            filename: __filename,
+        },
+        async(Void, citel, text) => {
+            if (!text) return citel.reply("Provide me a query!")
+            if (!text) return reply("Hey bie please tell me for which pic you're looking");
+            let name1 = text.split("|")[0]
+            let name2 = text.split("|")[10] || `5`
+            citel.reply(`Sending ${name2} image(s) of ${name1} in chat`)
+            let nn = name2
+            for (let i = 0; i < nn; i++) {
+
+                let n = await gis(name1)
+                images = n[Math.floor(Math.random() * n.length)].url;
+                    let buttonMessage = {
+                        image: {
+                            url: images,
+                        },
+                        caption: `_📸 Dark shan Image Search 📸_\n*${name1}*`,
+                        headerType: 4,
+                    };
+                    Void.sendMessage(citel.chat, buttonMessage, {
+                        quoted: citel,
+                    });
+            }
+        }
+    )
