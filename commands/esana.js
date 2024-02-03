04c6a7bd97a85cfbb6d528e07366ef8666f22066
kushansewmina1234 <115915441+kushansewmina1234@users.noreply.github.com>
2024-02-03 12:04:43 +0530

    Create esana.js

---
+++ commands/esana.js
@@ -0,0 +1,44 @@
+import Esana from '@sl-code-lords/esana-news';
+
+
+const api = new Esana();
+
+let handler = async (m, { conn }) => {
+  try {
+    const [command, newsId] = m.text.split(' ');
+
+    if (command === '.esana' && !newsId) {
+      await sendNewsList(m, conn);
+    } else if (command === '.esana' && newsId) {
+      await sendSpecificNews(newsId, m, conn);
+    }
+  } catch (error) {
+    console.error(error);
+    m.react('❌');
+  }
+};
+
+async function sendNewsList(m, conn) {
+}
+
+async function sendSpecificNews(newsId, m, conn) {
+  const newsInfo = await api.news(newsId);
+
+  const newsCaption = `•───── 💝 DARK SHAN 💝 ─────•
+❖ 𝑺𝑻𝑨𝑻𝑼𝑺: Active
+㋡ 𝑪𝑹𝑬𝑨𝑻𝑶𝑹: KUSHAN
+☞ 𝑵𝑬𝑾𝑺: ${newsInfo.results.TITLE}
+📅 Date: ${newsInfo.results.PUBLISHED}
+🔗 URL: ${newsInfo.results.URL}
+🗞️ Description: ${newsInfo.results.full_news}
+•───── 💝 DARK SHAN 💝 ─────•`;
+
+  conn.sendFile(m.chat, newsInfo.results.COVER, 'thumbnail.jpg', newsCaption, m);
+  m.react('✅');
+}
+
+handler.help = ['esana'];
+handler.tags = ['news'];
+handler.command = ['esana'];
+
+export default handler;
