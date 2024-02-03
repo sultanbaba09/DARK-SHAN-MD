021694ef37a33b737e2f1705e52ae63d0c8a589a
kushansewmina1234 <115915441+kushansewmina1234@users.noreply.github.com>
2024-02-03 19:23:44 +0530

    Update esana.js

--- commands/esana.js
+++ commands/esana.js
@@ -8,42 +8,31 @@ cmd({
             filename: __filename,
             use: '<helakuru>',
         },
+        async(Void, citel, text) => {
+            if (!text) return citel.reply("please use esana");
+            try {
+                 const URL = `https://www.helakuru.lk/esana/${last_id}`;
+                fetch(URL, {
+                        method: 'POST'
+                    })
+                    .then(response => response.json())
+                    .then(json => {
+                        const date = json.current_date;
+                        console.log(date);
+                        let textw = "";
+                        textw +=;      
+                        textw += `❖ 𝑺𝑻𝑨𝑻𝑼𝑺: Active`;
+                        textw += `㋡ 𝑪𝑹𝑬𝑨𝑻𝑶𝑹: KUSHAN`;
+                        textw += `☞ 𝑵𝑬𝑾𝑺: ${title}`;
+                        textw += `📅 Date: ${published}`;
+                        textw += `🔗 URL: ${url}`;
+                        textw += `🗞️ Description: ${full_news}`;
+                        citel.reply(textw)
+                    });
 
-
-
-const api = new Esana();
-
-let handler = async (m, { conn }) => {
-  try {
-    const [command, newsId] = m.text.split(' ');
-
-    if (command === '.esana' && !newsId) {
-      await sendNewsList(m, conn);
-    } else if (command === '.esana' && newsId) {
-      await sendSpecificNews(newsId, m, conn);
-    }
-  } catch (error) {
-    console.error(error);
-    m.react('❌');
-  }
-};
-
-async function sendNewsList(m, conn) {
-}
-
-async function sendSpecificNews(newsId, m, conn) {
-  const newsInfo = await api.news(newsId);
-
-  const newsCaption = `•───── 💝 DARK SHAN 💝 ─────•
-❖ 𝑺𝑻𝑨𝑻𝑼𝑺: Active
-㋡ 𝑪𝑹𝑬𝑨𝑻𝑶𝑹: KUSHAN
-☞ 𝑵𝑬𝑾𝑺: ${newsInfo.results.TITLE}
-📅 Date: ${newsInfo.results.PUBLISHED}
-🔗 URL: ${newsInfo.results.URL}
-🗞️ Description: ${newsInfo.results.full_news}
-•───── 💝 DARK SHAN 💝 ─────•`;
-
-  conn.sendFile(m.chat, newsInfo.results.COVER, 'thumbnail.jpg', newsCaption, m);
-  m.react('✅');
-}
+            } catch (e) {
+                console.log(e)
+            }
+        }
+    )
 //---------------------------------------------------------------------------
