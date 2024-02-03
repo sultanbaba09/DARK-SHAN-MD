fbb86897fa0627e3037cf32e56ea0c47f2a45ce5
kushansewmina1234 <115915441+kushansewmina1234@users.noreply.github.com>
2024-02-03 10:47:11 +0530

    🍁

--- commands/song.js
+++ commands/song.js
@@ -1,51 +1,67 @@
 const { tlang, cmd,fetchJson, botpic,ffmpeg, getBuffer, prefix, Config } = require('../lib')
 const ytdl = require('ytdl-secktor')
-cmd({
-            pattern: "song",
-            desc: "download yt song",
-            category: "downloader",
-            filename: __filename,
-            use: '<text>',
- },
-        async(Void, citel, text) => {
-            let yts = require("secktor-pack");
-            let search = await yts(text);
-            let anu = search.videos[0];
-            const getRandom = (ext) => {
-                return `${Math.floor(Math.random() * 10000)}${ext}`;
-            };
-            let infoYt = await ytdl.getInfo(anu.url);
-            if (infoYt.videoDetails.lengthSeconds >= videotime) return citel.reply(`😔 Video file too big!`);
-            let titleYt = infoYt.videoDetails.title;        { quoted: message.data, mimetype: 'audio/mpeg', fileName: `${title}.mp3` },
-        'audio'
-      )
+ Module_Exports({
+        pattern: "song",
+        desc: "Downloads audio from youtube.",
+        category: "downloader",
+        filename: __filename,
+        use: 'bella ciao',
+    },
+    async(bot, citel, tax) => {
+        let yts = require("secktor-pack");
+    if (!tax) return citel.send(`Example: ${prefix}song My Babe i love Your Voice`);
+        let search = await yts(tax);
+        let anu = search.videos[0];
+        const getRandom = (ext) => {
+            return `${Math.floor(Math.random() * 10000)}${ext}`;
+        };
+        let infoYt = await ytdl.getInfo(anu.url);
+        if (infoYt.videoDetails.lengthSeconds >= videotime) return citel.reply(`Audio File Too Big!`);
+        let titleYt = infoYt.videoDetails.title;
+        let randomName = getRandom(".mp3");
+        citel.reply('*𝙳𝙾𝚆𝙽𝙻𝙾𝙰𝙳𝙸𝙽𝙶:* '+tax)
+        const stream = ytdl(anu.url, {
+                filter: (info) => info.audioBitrate == 160 || info.audioBitrate == 128,
+            })
+            .pipe(fs.createWriteStream(`./${randomName}`));
+        await new Promise((resolve, reject) => {
+            stream.on("error", reject);
+            stream.on("finish", resolve);
+        });
+
+        let stats = fs.statSync(`./${randomName}`);
+        let fileSizeInBytes = stats.size;
+        let fileSizeInMegabytes = fileSizeInBytes / (1024 * 1024);
+        if (fileSizeInMegabytes <= dlsize) {
+            let buttonMessage = {
+                audio: fs.readFileSync(`./${randomName}`),
+                mimetype: 'audio/mpeg',
+                caption: sgen,
+                fileName: titleYt + ".mp3",
+                headerType: 4,
+                contextInfo: {
+                    externalAdReply: {
+                        title: snam,
+                        body: `sᴏɴɢ ᴅᴏᴡɴʟᴏᴀᴅᴇʀ`,
+                        renderLargerThumbnail: true,
+                        thumbnailUrl: search.all[0].thumbnail,
+                        mediaUrl: tax,
+                        mediaType: 1,
+                        thumbnail: await getBuffer(search.all[0].thumbnail),
+                        sourceUrl: zyt,
+                        
+                    },
+                },
+            }
+            await bot.sendMessage(citel.chat, buttonMessage, { quoted: citel })
+            return fs.unlinkSync(`./${randomName}`);
+        } else {
+            citel.reply(`File Size Bigger Then 100MB.`);
+        }
+        fs.unlinkSync(`./${randomName}`);
+        
+
+
     }
-    const result = await yts(match, 0, 1)
-    if (!result.length) return await message.send(`_Not result for_ *${match}*`)
-    const msg = generateList(
-      result.map(({ title, id, duration, author, album }) => ({
-        _id: `🆔&id\n`,
-        text: `🎵${title}\n🕒${duration}\n👤${author}\n📀${album}\n\n`,
-        id: `song https://www.youtube.com/watch?v=${id}`,
-      })),
-      `Searched ${match} and Found ${result.length} results\nsend 🆔 to download song.\n`,
-      message.jid,
-      message.participant
-    )
-    return await message.send(msg.message, { quoted: message.data }, msg.type)
-    // return await message.send(
-    // 	genListMessage(
-    // 		result.map(({ title, id, duration }) => ({
-    // 			text: title,
-    // 			id: `song https://www.youtube.com/watch?v=${id}`,
-    // 			desc: duration,
-    // 		})),
-    // 		`Searched ${match}\nFound ${result.length} results`,
-    // 		'DOWNLOAD'
-    // 	),
-    // 	{},
-    // 	'list'
-    // )
-  }
 )
-//---------------------------------------------------------------------------
+    
