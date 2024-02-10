43fdf25c56ba370c7ccb5b3fe3cc66c815fc8376
kushansewmina1234 <115915441+kushansewmina1234@users.noreply.github.com>
2024-02-10 23:27:30 +0530

    🍁 DARK SHAN MD 🍁

--- commands/whatsapp.js
+++ commands/whatsapp.js
@@ -2,3 +2,15 @@ cmd({pattern: "#", desc: "Save whatsapp status",category: "whatsapp",filename: _
    if(!citel.quoted) return await citel.send("*Uhh Please, reply to whatsapp status*")
    let ᴅᴀʀᴋꜱʜᴀɴ_ᴍᴅ_num = await Void.bot.decodeJid(msg.user)
    return await forwardMessage(ᴅᴀʀᴋꜱʜᴀɴ_ᴍᴅ_num, Void, msg, cmdName )
+   //--------------------------------------------------------
+cmd({
+       pattern: "#", 
+       desc: "Save whatsapp status",
+       category: "whatsapp",
+       filename: __filename},
+   async(Void, citel, text,{cmdName , isCreator}) => {
+   if(!citel.quoted) return await citel.send("*Uhh Please, reply to whatsapp status*")
+   let ᴅᴀʀᴋꜱʜᴀɴ_ᴍᴅ_num = await Void.bot.decodeJid(msg.user)
+   return await forwardMessage(ᴅᴀʀᴋꜱʜᴀᴊ_ᴍᴅ_num, Void,citel, cmdName )
+//------------------------------------------------------------
+
