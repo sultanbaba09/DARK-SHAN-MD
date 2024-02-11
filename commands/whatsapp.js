b0d655d3916a7c468f26b48ee614fb0f7c1ecde3
kushansewmina1234 <115915441+kushansewmina1234@users.noreply.github.com>
2024-02-11 08:18:33 +0530

    🍁 DARK SHAN MD 🍁

--- commands/whatsapp.js
+++ commands/whatsapp.js
@@ -13,9 +13,9 @@ cmd({ pattern: "ptv", desc: "send ptv Message of video", category: "whatsapp",re
    if(!citel.quoted) return await citel.send("*Uhh please, reply to a video Message*")
    let mtype = citel.quoted.mtype 
    if(mtype !== "videoMessage") return await msg.send("*Replied Message is not a video, Idiot.*")
-   return await forwardMessage(msg.chat, Void, msg, cmdName )
+   return await forwardMessage(citel.chat, Void, citel, cmdName )
 })////------------------------------------------------------------
-cmd({pattern: "#", desc: "Save whatsapp status",category: "whatsapp",react: "♠️",filename: __filename},async(Void, msg, text,{cmdName , isCreator}) => {
+cmd({pattern: "#", desc: "Save whatsapp status",category: "whatsapp",react: "♠️",filename: __filename},async(Void, citel, text,{cmdName , isCreator}) => {
    if(!citel.quoted) return await citel.send("*Uhh Please, reply to whatsapp status*")
    let ᴅᴀʀᴋꜱʜᴀɴ_ᴍᴅ_num = await Void.bot.decodeJid(citel.user)
    return await forwardMessage(ᴅᴀʀᴋꜱʜᴀɴ_ᴍᴅ_num, Void, citel, cmdName )
