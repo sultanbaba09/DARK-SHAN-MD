37703b18ba279cfe2c5acbc798bacc940c35cff3
kushansewmina1234 <115915441+kushansewmina1234@users.noreply.github.com>
2024-02-17 10:33:33 +0530

    Create Song2.js

---
+++ commands/Song2.js
@@ -0,0 +1,43 @@
+const { tlang, ringtone, cmd,fetchJson, sleep, botpic,ffmpeg, getBuffer, pinterest, prefix, Config } = require('../lib')
+const { mediafire } = require("../lib/mediafire.js");
+const googleTTS = require("google-tts-api");
+const ytdl = require('ytdl-secktor')
+const fs = require('fs-extra')
+var videotime = 60000 // 1000 min
+var dlsize = 1000 // 1000mb
+
+
+
+cmd({
+        pattern: "yts",
+        desc: "Gives descriptive info of query from youtube..",
+        react: "⌛️",
+        category: "dark shan",
+        filename: __filename,
+        use: '<yt search text>',
+    },
+    async(Void, citel, text) => {
+        let yts = require("secktor-pack");
+        if (!text) return citel.reply(`Example : ${prefix}yts ${tlang().title} WhatsApp Bot`);
+        let search = await yts(text);
+        let textt = "*YouTube Search*\n\n Result From " + text + "\n\n───────────────────\n";
+        let no = 1;
+        for (let i of search.all) {
+            textt += `${no++}\n ✏️Title : ${i.title}\n
+    }\n🙈Views : ${i.views}\n⌛Duration : ${
+      i.timestamp
+    }\n🎵Url : ${
+      i.url
+    }\n\n\n\n`;
+		
+        }
+        return Void.sendMessage(citel.chat, {
+            image: {
+                url: search.all[0].thumbnail,
+            },
+            caption: textt,
+        }, {
+            quoted: citel,
+        });
+    }
+)
