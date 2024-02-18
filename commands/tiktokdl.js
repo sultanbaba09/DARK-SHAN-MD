d8b0b1422d203585e11c906684764c17540b4926
kushansewmina1234 <115915441+kushansewmina1234@users.noreply.github.com>
2024-02-18 11:40:58 +0530

    🍁 DARK SHAN MD 🍁

---
+++ commands/tiktokdl.js
@@ -0,0 +1,39 @@
+const { cmd, fetchJson } = require('../lib');
+
+cmd(
+    {
+        pattern: "td",
+        alias: ['kk'],
+        category:"downloader",
+        react: "📽️",
+        filename: __filename
+    },
+    async (Void, citel, text) => {
+        try {
+            if (!text) {
+                citel.reply("*Please provide a valid URL* ✏️.");
+                return;
+            }
+
+            const wamod = await fetchJson(`https://api.neoxr.eu/api/tiktok?url=${url}&apikey=KjAtqH`);
+
+            if (!wamod.result || !wamod.result.hd) {
+                citel.reply("Failed to fetch video URL or HD link ❌.");
+                return;
+            }
+            
+            await Void.sendMessage(
+                citel.chat,
+                {
+                    video: { url: wamod.result.hd },
+                    mimetype: "video/mp4",
+                    caption:'┏━━━━━━━━━━━━━┓\n\n🐹 * DARK SHAN MD  TIKTOK  ᴅᴏᴡɴʟᴏᴀᴅᴇʀ*🐹\n\n▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁\n\n✷▎🎋⃟🥷 *ᴄʀᴇᴀᴛᴇʀ*: kushansewmina\n\n✷▎🎋⃟🥷 *ᴄʀᴇᴀᴛᴇʀ ɴᴜᴍʙᴇʀ*:  wa.me//+9477xxxxxx\n\n┗━━━━━━━━━━━━━┛\n\n*ʏᴏᴜʀ ᴠɪᴅᴇᴏ ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ ᴘʟᴇᴀꜱᴇ ᴡᴀɪᴛ*...🔥🔥'
+                },
+                { quoted: citel }
+            );
+
+        } catch (error) {
+            citel.reply("An error occurred: " + error.message);
+        }
+    }
+);
