4d3c5ad53d40b5c33172287502a1e296f93c258d
kushansewmina1234 <115915441+kushansewmina1234@users.noreply.github.com>
2024-02-19 15:15:52 +0530

    Update tiktokdl.js

--- commands/tiktokdl.js
+++ commands/tiktokdl.js
@@ -17,7 +17,7 @@ cmd(
 
             const data = await fetchJson(`https://vihangayt.me/download/tiktok?url=${text}`);
 
-            if (!data.result || !data.result.mp4) {
+            if (!data.result || !data.result.1) {
                 citel.reply("Failed to fetch video URL or HD link ❌.");
                 return;
             }
@@ -25,7 +25,7 @@ cmd(
             await Void.sendMessage(
                 citel.chat,
                 {
-                    video: { url: data.result.mp4},
+                    video: { url: data.result.1},
                     mimetype: "video/mp4",
                     caption:'┏━━━━━━━━━━━━━┓\n\n🐹 * DARK SHAN MD  TIKTOK  ᴅᴏᴡɴʟᴏᴀᴅᴇʀ*🐹\n\n▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁\n\n✷▎🎋⃟🥷 *ᴄʀᴇᴀᴛᴇʀ*: kushansewmina\n\n✷▎🎋⃟🥷 *ᴄʀᴇᴀᴛᴇʀ ɴᴜᴍʙᴇʀ*:  wa.me//+9477xxxxxx\n\n┗━━━━━━━━━━━━━┛\n\n*ʏᴏᴜʀ ᴠɪᴅᴇᴏ ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ ᴘʟᴇᴀꜱᴇ ᴡᴀɪᴛ*...🔥🔥'
                 },
