f264cabf331e0b434c5a30705da84d5357af6ace
kushansewmina1234 <115915441+kushansewmina1234@users.noreply.github.com>
2024-02-20 19:03:23 +0530

    Update spotify.js

--- commands/spotify.js
+++ commands/spotify.js
@@ -1,6 +1,35 @@
 const { tlang, ringtone, cmd,fetchJson, sleep, botpic, getBuffer, pinterest, prefix, Config } = require('../lib')
 
-cmd 
+cmd( 
+    {   
+        pattern: "spotify",
+        alias: ["sp"],
+        category: "spotify",
+        react: "✨",
+        filename: __filename
+    },   
+    async (void, citel, text) => {
+        try {
+            if (!text) {
+                citel.reply("please enter song name");
+                return;
+            } 
+           
+            const data = await fetchjson(
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
