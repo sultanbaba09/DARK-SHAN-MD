02f8d85d9a4f02c890fdcf97fb79bfba81e379e8
kushansewmina1234 <115915441+kushansewmina1234@users.noreply.github.com>
2024-02-20 19:36:06 +0530

    🍁 DARK SHAN MD 🍁

--- commands/spotify.js
+++ commands/spotify.js
@@ -11,11 +11,33 @@ cmd(
     async (void, citel, text) => {
         try {
             if (!text) {
-                citel.reply("please enter song name");
+                citel.reply("please enter song name 🍍");
                 return;
             } 
            
             const data = await fetchjson(`https://vihangayt.me/search/spotify?q=${text}`);
+
+            if (!data.result || !data.result.preview) {
+                citel.reply("faild to fetch song 🌐");
+                return;
+            } 
+
+            await void.sendMessage(
+                clite.chat,
+                {
+                    song: {url: data.result.preview},
+                    mimetype: "audio/mpeg",
+                    caption: ' DARK SHAN MD AUDIO DOWNLOAD'
+                },
+                { quoted: citel }
+            );
+        } catch (error) {
+            citel.reply("an erro occurred: "erro.message);
+        }
+   }
+);
+                        
+                
 
 
 
