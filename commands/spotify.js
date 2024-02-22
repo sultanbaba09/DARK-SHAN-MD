6fa6ac3dd60cf97e96021203fa82da613d9b31b8
kushansewmina1234 <115915441+kushansewmina1234@users.noreply.github.com>
2024-02-22 18:40:07 +0530

    Update spotify.js

--- commands/spotify.js
+++ commands/spotify.js
@@ -1,26 +1,31 @@
-const { tlang, ringtone, cmd,fetchJson, sleep, botpic, getBuffer, pinterest, prefix, Config } = require('../lib')
+const moment = require('moment-timezone')
+const {fetchJson,cmd, tlang } = require('../lib')
+let gis = require("async-g-i-s");
+const axios = require('axios')
+const fetch = require('node-fetch')
 
-cmd({
+
+    cmd({
         pattern: "bing",
-        category: "test",
-        desc: "Sends info of given query from microsoft Search.",
+        alias :['ff','microsoft'],
+        category: "search",
+        desc: "Sends info of given query from Google Search.",
         use: '<text>',
         filename: __filename,
     },
- 
     async(Void, citel, text) => {
-        if (!text) return citel.reply(`give me a query\n*Example : .google Who is Suhail Tech.*`);
-        const bing = await fetchJson(`https://rest-api.akuari.my.id/search/bingsearch?query=${text}`);
-        let search = await bing(text)
-        let textt = "*YouTube Search*\n\n Result From " + text + "\n\n───────────────────\n";
-        for (let b of search.all){
-                textt+= `➣ Title : ${b.title}\n`;
-                textt+= `➣ Description : ${b.description}\n`;
-                textt+= `➣ Link : ${b.url}\n\n────────────────────────\n\n`;
+        if (!text) return citel.reply(`give me a query\n*Example : .bing Who is dark shan yt.*`);
+        let bing = await axios.get(`https://rest-api.akuari.my.id/search/bingsearch?query=${text}`);
+        bing({ 'query': text}).then(res => {
+            let msg= `bing Search From : ${text} \n\n`;
+            for (let b of res) {
+                msg+= `➣ Title : ${b.title}\n`;
+                msg+= `➣ link : ${b.url}\n`;
+                msg+= `➣ Description : ${b.description}\n\n────────────────────────\n\n`;
             }
          
             return citel.reply(msg);
         })
     }
-) 
+)
 //-----------------------------------------------------------------------//
