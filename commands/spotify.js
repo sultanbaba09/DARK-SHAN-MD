c1a3b4bd5a59cd5d2848140bb77fb3c8eb525f09
kushansewmina1234 <115915441+kushansewmina1234@users.noreply.github.com>
2024-02-24 10:53:21 +0530

    Update spotify.js

--- commands/spotify.js
+++ commands/spotify.js
@@ -1,41 +1,41 @@
-const fetch = require("node-fetch")
+//const fetch = require("node-fetch")
 
-cmd({
+//cmd({
+//
+//            pattern: "spotify",
+//            desc: "video dl",
+//            react: "📽️",
+//            category: "downloader",
+//            filename
+//
+//        },
 
-            pattern: "spotify",
-            desc: "video dl",
-            react: "📽️",
-            category: "downloader",
-            filename
-
-        },
-
-        async(Void, citel, text) => {    
-            if (!text) return citel.reply("Provide me a name")
-            try {
-                const shan = `https://vihangayt.me/download/spotify?url=${text}`;
-                fetch(shan)
-                    .then (sdata => shan.json());
-
-            if (!sdata.data || !sdata.data.url) {
-                citel.reply("Failed to fetch video URL link ❌.");
-                return;
-            }
-            
-            await Void.sendMessage(
-                citel.chat,
-                {
-                    audio: { url: sdata.data.url },
-                    mimetype: "audio/mpeg",
-                },
-                { quoted: citel }
-            );
-
-        } catch (error) {
-            citel.reply("An error occurred: " + error.message);
-        }
-    }
-);
+//        async(Void, citel, text) => {    
+//            if (!text) return citel.reply("Provide me a name")
+//            try {
+//                const shan = `https://vihangayt.me/download/spotify?url=${text}`;
+//                fetch(shan)
+//                    .then (sdata => shan.json());
+//
+//            if (!sdata.data || !sdata.data.url) {
+//                citel.reply("Failed to fetch video URL link ❌.");
+//                return;
+//            }
+//            
+//            await Void.sendMessage(
+//                citel.chat,
+//                {
+//                    audio: { url: sdata.data.url },
+//                    mimetype: "audio/mpeg",
+//                },
+//                { quoted: citel }
+//            );
+//
+//        } catch (error) {
+//            citel.reply("An error occurred: " + error.message);
+//        }
+//    }
+//);
 
                     
                         
