a5d02898e62a5b0c15f37e9a2c83433fa0dc1e15
kushansewmina1234 <115915441+kushansewmina1234@users.noreply.github.com>
2024-02-24 11:28:22 +0530

    Update spotify.js

--- commands/spotify.js
+++ commands/spotify.js
@@ -1,44 +1,5 @@
 
-const { cmd, fetchJson } = require('../lib');
+erro 404 nod found 
 
-cmd(
-    {
-        pattern: "spotify",
-        alias: ['st'],
-        category:"shan",
-        react: "📽️",
-        filename: __filename
-    },
-    async (Void, citel, text) => {
-        try {
-            if (!text) {
-                citel.reply("*Please provide a valid URL* ✏️.");
-                return;
-            }
-
-            const shan = await fetchJson(`https://vihangayt.me/download/spotify?url=${text}`);
-
-            if (!shan.data || !shan.data.url) {
-                citel.reply("Failed to fetch video URL or HD link ❌.");
-                return;
-            }
-            
-            await Void.sendMessage(
-                citel.chat,
-                {
-                    audio: { url: shan.data.url },
-                    mimetype: "audio/mpeg",
-                },
-                { quoted: citel }
-            );
-
-        } catch (error) {
-            citel.reply("An error occurred: " + error.message);
-        }
-    }
-);
-
-//----------------------------------------------------------------------------------------// 
-                    
                         
                                
