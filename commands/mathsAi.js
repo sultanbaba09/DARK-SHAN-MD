a08cfc3976a2bc0aade715ee76341f6574b67ac1
kushansewmina1234 <115915441+kushansewmina1234@users.noreply.github.com>
2024-02-26 12:36:52 +0530

    Update mathsAi.js

--- commands/mathsAi.js
+++ commands/mathsAi.js
@@ -2,62 +2,7 @@ const {fetchJson,cmd, tlang } = require('../lib')
 const axios = require('axios')
 const fetch = require('node-fetch')
 
-cmd({
-            pattern: "maths",
-            category: "maths AI",
-            desc: "maths solver",
-            use: '<location>',
-            filename: __filename,
-    },
-        async(Void, citel, text) => {
-            if (!text) return citel.reply("example:- 20×20");
-   
-            let mdata = await axios.get(`https://vihangayt.me/tools/mathssolve?q=${text}`);
- 
-            let textw = "";
-  
-            textw += `${mdata.data}`;
 
-            Void.sendMessage(
-                citel.chat, {
-                    text: textw,
-                }, {
-                    quoted: citel,
-                }
-            );
-
-        }
-    )
-//----------------------------------------------------------------------------//
-
-cmd(
-    {
-        pattern: "ai4",
-        category:"dark",
-        react: "🛞",
-        filename: __filename
-    },
-    async (Void, citel, text) => {
-                if (!text) return citel.reply("give me text");
-   
-            let gdata = await axios.get(`https://vihangayt.me/tools/chatgpt?q=${text}`);
- 
-            let dark = "";
-  
-            dark += `${gdata.data}`;
-
-            Void.sendMessage(
-                citel.chat, {
-                    text: textw,
-                }, {
-                    quoted: citel,
-                }
-            );
-
-        }
-    )
-
-//------------------------------------------
 
 cmd(
     {
@@ -81,3 +26,31 @@ text +="\n*DARK SHAN MD:* " + gpt.data;
 
 
 })
+
+
+//cmd({
+//            pattern: "maths",
+//            category: "maths AI",
+//            desc: "maths solver",
+//            use: '<location>',
+//            filename: __filename,
+//    },
+//        async(Void, citel, text) => {
+//            if (!text) return citel.reply("example:- 20×20");
+//   
+//            let mdata = await axios.get(`https://vihangayt.me/tools/mathssolve?q=${text}`);
+// 
+//            let textw = "";
+// 
+//            textw += `${mdata.data}`;
+//
+//            Void.sendMessage(
+//                citel.chat, {
+//                   text: textw,
+//                }, {
+//                    quoted: citel,
+//                }
+//            );
+//
+//        }
+//    )
