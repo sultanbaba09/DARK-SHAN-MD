308a9c7f10a8ae2800dc2ba7e5638224d98e118a
kushansewmina1234 <115915441+kushansewmina1234@users.noreply.github.com>
2024-02-26 07:38:33 +0530

    🍁 DARK SHAN MD 🍁

--- commands/mathsAi.js
+++ commands/mathsAi.js
@@ -7,9 +7,10 @@ cmd({
             desc: "maths solver",
             use: '<location>',
             filename: __filename,
-        },
+    },
         async(Void, citel, text) => {
             if (!text) return citel.reply("example:- 20×20");
+   
             let mdata = await axios.get(`https://vihangayt.me/tools/mathssolve?q=${text}`);
  
             let textw = "";
@@ -26,4 +27,44 @@ cmd({
 
         }
     )
+//----------------------------------------------------------------------------//
+
+cmd ({    
+            pattern: "gpt4",
+            category: "ai",
+            desc: "chatgpt 4",
+            filename: __filename,
+
+     },
+     async(Void, citel, text) => {
+                 try{
+                    if (!text) {
+                       citel.reply(`example :- ${prefix}gpt4 hi`);
+                                return;
+                    }
+
+                    const gdata fetchJson(`https://vihangayt.me/tools/chatgpt?q=${text}`);
+
+                    if (!gdata.data|| !gdata.data) {
+                        citel.reply("Failed to shearch");
+                return;
+                    }   
+
+            await Void.sendMessage(
+                citel.chat,
+                {
+                       text: { data: gdata.data },
+
+                },
+                { quoted: citel }
+            );
+
+        } catch (error) {
+            citel.reply("An error occurred: " + error.message);
+        }
+    }
+);             
+                        
+                             
+
 
