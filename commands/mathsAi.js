9a518993f21418bd476e1ac888b4c64b419e969d
kushansewmina1234 <115915441+kushansewmina1234@users.noreply.github.com>
2024-02-26 12:52:54 +0530

    Update mathsAi.js

--- commands/mathsAi.js
+++ commands/mathsAi.js
@@ -3,54 +3,29 @@ const axios = require('axios')
 const fetch = require('node-fetch')
 
 
-
-cmd(
-    {
-        pattern: "ai3",
-        category:"dark",
-        react: "🛞",
-        filename: __filename
+cmd({
+            pattern: "maths",
+            category: "maths AI",
+            desc: "maths solver",
+            use: '<location>',
+            filename: __filename,
     },
-        async(Void, citel, text) => {
+       async(Void, citel, text) => {
+            if (!text) return citel.reply("example:- 20×20");
+ 
+           let mdata = await axios.get(`https://vihangayt.me/tools/mathssolve?q=${text}`);
+ 
+            let textw = "";
 
-          citel.reply (`*_Please Wait, Getting Cricket Info_*`);
-const darkshan = await fetch(`https://vihangayt.me/tools/chatgpt?q=${text}`);
-  const gpt = await darkshan.json();
-console.log(gpt);
+            textw += `${mdata.data}`;
 
-let text = "";
-text +="\n*DARK SHAN MD:* " + gpt.data;
+            Void.sendMessage(
+                citel.chat, {
+                   text: textw,
+                }, {
+                    quoted: citel,
+                }
+            );
 
-}
- return await citel.reply( text);
-
-
-})
-
-
-//cmd({
-//            pattern: "maths",
-//            category: "maths AI",
-//            desc: "maths solver",
-//            use: '<location>',
-//            filename: __filename,
-//    },
-//        async(Void, citel, text) => {
-//            if (!text) return citel.reply("example:- 20×20");
-//   
-//            let mdata = await axios.get(`https://vihangayt.me/tools/mathssolve?q=${text}`);
-// 
-//            let textw = "";
-// 
-//            textw += `${mdata.data}`;
-//
-//            Void.sendMessage(
-//                citel.chat, {
-//                   text: textw,
-//                }, {
-//                    quoted: citel,
-//                }
-//            );
-//
-//        }
-//    )
+        }
+    )
