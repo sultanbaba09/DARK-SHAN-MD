53075b8d13c5fb6e2087006657b8eecf584b1f1b
kushansewmina1234 <115915441+kushansewmina1234@users.noreply.github.com>
2024-02-27 17:48:55 +0530

    Update fb.js

--- commands/fb.js
+++ commands/fb.js
@@ -26,7 +26,7 @@ cmd(
                 citel.chat,
                 {
                     video: { url: shan.result.hd },
-                    mimetype: "video/mp4",
+                    
                     caption:'┏━━━━━━━━━━━━━┓\n\n🐹 * DARK SHAN MD  ꜰʙ ᴠɪᴅᴇᴏ ᴅᴏᴡɴʟᴏᴀᴅᴇʀ*🐹\n\n▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁\n\n✷▎🎋⃟🥷 *ᴄʀᴇᴀᴛᴇʀ*: kushansewmina\n\n✷▎🎋⃟🥷 *ᴄʀᴇᴀᴛᴇʀ ɴᴜᴍʙᴇʀ*:  wa.me//+9477xxxxxx\n\n┗━━━━━━━━━━━━━┛\n\n*ʏᴏᴜʀ ᴠɪᴅᴇᴏ ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ ᴘʟᴇᴀꜱᴇ ᴡᴀɪᴛ*...🔥🔥'
                 },
                 { quoted: citel }
