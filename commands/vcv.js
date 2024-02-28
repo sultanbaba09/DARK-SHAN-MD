5fb4daf6f67df4164909ddb5a01c50b08d260e8f
kushansewmina1234 <115915441+kushansewmina1234@users.noreply.github.com>
2024-02-28 18:27:51 +0530

    Update vcv.js

--- commands/vcv.js
+++ commands/vcv.js
@@ -20,22 +20,23 @@ const fetch = require('node-fetch')
 cmd({
             pattern: "sps",
             react: "💿️",
-            category: "search",
+            category: "check",
             desc: "Sends info of given query from Google Search.",
             use: '<text>',
             filename: __filename,
         },
         async(Void, citel, text) => {
 
-
+          citel.reply (`*_Please Wait, Getting Cricket Info_*`);
 const response = await fetch(`https://vihangayt.me/search/spotify?q=${text}`);
   const sps = await response.json();
+console.log(sps);
       
         for (let i=1; i<5; i++){
-  textw+=`
-TITLE : ${sps.data[i].title}
-DURATION : ${sps.data[i].duration}
-URL : ${sps.data[i].url}\n`
+  
+textw+= `TITLE : ${sps.data[i].title}`;
+textw+= `DURATION : ${sps.data[i].duration}`;
+textw+= ` URL : ${sps.data[i].url}\n`
         }
 
  return await citel.reply(textw );
