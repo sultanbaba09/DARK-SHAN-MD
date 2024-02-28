930a7bb6af40235690e378a2f5bbb3a7f6f1848c
kushansewmina1234 <115915441+kushansewmina1234@users.noreply.github.com>
2024-02-28 20:45:18 +0530

    Create spotifys.js

---
+++ commands/spotifys.js
@@ -0,0 +1,33 @@
+const moment = require('moment-timezone')
+const {fetchJson,cmd, tlang } = require('../lib')
+let gis = require("async-g-i-s");
+const axios = require('axios')
+const fetch = require('node-fetch')
+
+//---------------------------------------------------------------------------
+
+cmd({
+            pattern: "spo",
+            react: "💿️",
+            category: "check",
+            desc: "Sends info of given query from Google Search.",
+            use: '<text>',
+            filename: __filename,
+        },
+        async(Void, citel, text) => {
+
+const response = await fetch(`https://vihangayt.me/search/spotify?q=${text}`);
+  const sps = await response.json();
+console.log(sps);
+      
+        for (let i=1; i<5; i++){
+  
+textw +=`TITLE : ${sps.data[i].title}`;
+textw +=`DURATION : ${sps.data[i].duration}`;
+textw +=`URL : ${sps.data[i].url}\n`
+        }
+
+ return await citel.reply(textw );
+
+
+})
