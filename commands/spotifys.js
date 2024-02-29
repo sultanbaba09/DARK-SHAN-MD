ea9e1f74d678af1a4bd9203165e025855a620fbe
kushansewmina1234 <115915441+kushansewmina1234@users.noreply.github.com>
2024-02-29 09:56:17 +0530

    Update spotifys.js

--- commands/spotifys.js
+++ commands/spotifys.js
@@ -20,7 +20,7 @@ cmd({
 const response = await fetch(`https://vihangayt.me/search/spotify?q=${text}`);
   const sps = await response.json();
 console.log(sps);
-                    let textw = `spotify search results\n\n`;
+                    let textw = `◉┉❮❮ 𝙳𝙰𝚁𝙺 𝚂𝙷𝙰𝙽 𝙼𝙳 𝚂𝙿𝙾𝚃𝙸𝙵𝚈 𝚂𝙷𝙴𝙰𝚁𝙲𝙷 ❯❯┉◉\n\n`;
       
         for (let i=1; i<5; i++){
   
