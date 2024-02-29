c5b4848d247542ba554aa20b2121d5865c86603d
kushansewmina1234 <115915441+kushansewmina1234@users.noreply.github.com>
2024-02-29 16:35:58 +0530

    Update Ai.js

--- commands/Ai.js
+++ commands/Ai.js
@@ -2,40 +2,6 @@ const { cmd, fetchJson } = require('../lib');
 const fetch = require('node-fetch')
 
 
-cmd(
-    {
-        pattern: "ai",
-        category: "test",
-        react: "🌷",
-        filename: __filename
-    },
-    async (Void, citel, text) => {
-        try {
-            if (!text) {
-                citel.reply("*Please give me text.");
-                return;
-            }
-
-            const gpt = await fetchJson(`https://api.neoxr.eu/api/gpt?q=${text}`);
-
-            if (!gpt.data || !gpt.data.message) {
-                citel.reply("Failed to fetch ❌.");
-                return;
-            }
-            
-            await Void.sendMessage(
-                citel.chat,
-                {
-                    text: {q: gpt.data.message },
-                },
-                { quoted: citel }
-            );
-
-        } catch (error) {
-            citel.reply("An error occurred: " + error.message);
-        }
-    }
-);
 
 //---------------------------------------------------------------------//
 
@@ -58,6 +24,9 @@ console.log(gpt);
   
 
 textw +=`❒ URL : ${gpt.data.message}`
+
+            footer: tlang().footer,
+                headerType: 4,
         
 
  return await citel.reply(textw );
