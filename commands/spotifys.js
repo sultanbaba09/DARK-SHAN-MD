f7134d4735803fc3ea89717559f5bfafc1b958a1
kushansewmina1234 <115915441+kushansewmina1234@users.noreply.github.com>
2024-02-29 08:01:14 +0530

    Update spotifys.js

--- commands/spotifys.js
+++ commands/spotifys.js
@@ -7,7 +7,7 @@ const fetch = require('node-fetch')
 //---------------------------------------------------------------------------
 
 cmd({
-            pattern: "spo",
+            pattern: "𝚜𝚙𝚘",
             react: "💿️",
             category: "check",
             desc: "Sends info of given query from Google Search.",
@@ -19,13 +19,15 @@ cmd({
 const response = await fetch(`https://vihangayt.me/search/spotify?q=${text}`);
   const sps = await response.json();
 console.log(sps);
-                    let textw = `spotify search results\n\n`;
+                    let textw = `◉┉❮❮ 𝙳𝙰𝚁𝙺 𝚂𝙷𝙰𝙽 𝙼𝙳 𝚂𝙿𝙾𝚃𝙸𝙵𝚈 𝚂𝙷𝙴𝙰𝚁𝙲𝙷 ❯❯┉◉\𝚗\n\n`;
       
         for (let i=1; i<5; i++){
   
-textw +=`TITLE : ${sps.data[i].title}`;
-textw +=`DURATION : ${sps.data[i].duration}`;
-textw +=`URL : ${sps.data[i].url}\n`
+textw +=`🕯️ _*TITLE*_ : ${sps.data[i].title}\n\n`;
+textw +=`💹 _*DURATION*_ : ${sps.data[i].duration}\n\n`;
+textw +=`📌️ _*URL*_ : ${sps.data[i].url}\n\n`
+textw +=`📊 _*POPULARITY*_ : ${sps.data[i].popularity}\n\n`;
+textw +=`🖊️ _*ARTIST*_ : ${sps.data[i].artist}\n\n`;
         }
 
  return await citel.reply(textw );
