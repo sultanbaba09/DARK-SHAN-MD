cb3756468bf938cdc6f96e5830165cfc84606fa8
kushan sewmina <115915441+kushansewmina1234@users.noreply.github.com>
2024-03-03 21:51:32 +0530

    Update ss.js

--- commands/ss.js
+++ commands/ss.js
@@ -18,12 +18,10 @@ const response = await fetch(`https://vihangayt.me/search/soundcloud?q=${text}`)
 console.log(shan);
                     let textw = `◉┉❮❮ 𝙳𝙰𝚁𝙺 𝚂𝙷𝙰𝙽 𝙼𝙳 𝚂𝙾𝚄𝙽𝙳𝙲𝙻𝙾𝚄𝙳 𝚂𝙷𝙴𝙰𝚁𝙲𝙷 ❯❯┉◉\n\n`;
       
-        for (let i=1; i<4; i++){
+        for (let i=1; i<6; i++){
   
 textw +=`❒ TITLE : ${shan.result[i].title}\n`;
-textw +=`❒ DURATION : ${shan.result[i].timestamp}\n`;
-textw +=`❒ ARTIST : ${shan.result[i].artist}\n`;
-textw +=`❒ VIEWS : ${shan.result[i].views}\n`;         
+textw +=`❒ ARTIST : ${shan.result[i].artist}\n`;         
 textw +=`❒ URL : ${shan.result[i].url}\n\n`
         }
 
