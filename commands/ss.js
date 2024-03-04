ed2a10172536907b93401bd8ea259a900da6d939
kushan sewmina <115915441+kushansewmina1234@users.noreply.github.com>
2024-03-04 15:49:54 +0530

    Update ss.js

--- commands/ss.js
+++ commands/ss.js
@@ -1,1 +1,40 @@
-helohi
+
+const os = require('os');
+
+const { cmd,tlang,prefix,runtime,formatp} = require('../lib/');
+
+cmd({
+
+            pattern: "groupsettings",
+
+            alias: ["gs"],
+
+            desc: "some group settings change.",
+
+            category: "extra",
+
+            filename: __filename
+
+        },
+
+        async(Void, citel, text) => {
+
+           let list = [{
+
+title: 'MUTE GROUP 🔇',
+
+rowId: `${prefix}mute`,
+
+description: ' '
+
+},
+
+{
+
+title: 'UNMUTE GROUP 🔊',
+
+rowId: `${prefix}unmute`,
+
+description: ' '
+
+},
