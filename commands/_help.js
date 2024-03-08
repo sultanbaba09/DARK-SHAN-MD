daf08477bad22c8d95a72ca5fc364e9ef5cb19bf
kushan sewmina <115915441+kushansewmina1234@users.noreply.github.com>
2024-03-08 08:02:14 +0530

    Update _help.js

--- commands/_help.js
+++ commands/_help.js
@@ -60,9 +60,7 @@ Secktor.cmd({
 ` + '```'
                 for (const category in cmds) 
                 {
-                   str += `╭─────
-┇
-┻❲ *${tiny(category)}* ❳────╮\n` ;
+                   str += `╭────❲ *${tiny(category)}* ❳────╮\n┇\n┻\n` ;
                    if(text.toLowerCase() == category.toLowerCase()){ str = `╭────❲ *${tiny(category)}* ❳────╮\n┇\n┻\n` ;
     
                         for (const plugins of cmds[category]) { str += `│ ${fancytext(plugins,1)}\n` ; }
