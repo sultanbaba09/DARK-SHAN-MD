2eabe0dc05585e079ae50555e4f7e5013f5678ab
kushan sewmina <115915441+kushansewmina1234@users.noreply.github.com>
2024-03-30 14:30:19 +0530

    Update fbdl2.js

--- commands/fbdl2.js
+++ commands/fbdl2.js
@@ -8,23 +8,10 @@ cmd({
         filename: __filename,
     },
     async(Void, citel) => {
-        let { data } = await axios.get('https://api.github.com/repos/kushansewmina1234/DARK-SHAN-MD')
-        let cap = `💈 *DARK SHAN MD REPO* \n
-*⭐ Total Stars:* ${data.stargazers_count} stars
-
-*🍽️ Forks:* ${data.forks_count} forks
-
-*🍁 Repo:* https://github.com/kushansewmina1234/DARK-SHAN-MD
-🎏️️ *Group:*
- https://whatsapp.com/channel/0029VaFLAgi90x2oD70Hwq1z
-
-🀄 *developer:*-KUSHAN SEWMINA
-
-
-©️ *DARK SHAN MD 2024*`
+        
       
 
-          Void.sendPoll(citel.chat, `${cap}`, ['.menu', '.ping']),
+          Void.sendPoll(citel.chat, 'please choice one' , ['.menu', '.ping']),
            {
             quoted: citel,
         });
