eee2a4792003219d580f1a71297619291d04d584
kushan sewmina <115915441+kushansewmina1234@users.noreply.github.com>
2024-03-30 12:57:18 +0530

    Update fbdl2.js

--- commands/fbdl2.js
+++ commands/fbdl2.js
@@ -1,56 +1,41 @@
 const { cmd, fetchJson } = require('../lib');
 
 
-
-      
-      
 cmd({
+        pattern: "repo",
+        alias: ["git", "sc", "script"],
+        desc: "Sends info about repo.",
+        category: "general",
+        filename: __filename,
+    },
+    async(Void, citel) => {
+        let { data } = await axios.get('https://api.github.com/repos/kushansewmina1234/DARK-SHAN-MD')
+        let cap = `💈 *DARK SHAN MD REPO* \n
+*⭐ Total Stars:* ${data.stargazers_count} stars
 
-            pattern: "polll",           
-            desc: "(menu cmdlist).",
-            category: "_help",
-            react: "👨‍💻",
-            filename: __filename,
-            use: '<faded-Alan walker.>',
+*🍽️ Forks:* ${data.forks_count} forks
 
-        },
+*🍁 Repo:* https://github.com/kushansewmina1234/DARK-SHAN-MD
+🎏️️ *Group:*
+ https://whatsapp.com/channel/0029VaFLAgi90x2oD70Hwq1z
 
-        async(Void, citel, text) => {
-        
-            let buttonMessage = {
-                
-                        {
+🀄 *developer:*-KUSHAN SEWMINA
 
-const githubRepoUrl = 'https://api.github.com/repos/gssbotwa/Gssbotwa2';
 
-            }},
+©️ *DARK SHAN MD 2024*`
+        let buttonMessaged = {
+            caption: cap,
             
+        };
+
+          Void.sendPoll(citel.chat, uy, ['.menu', '.ping'],
+        return await Void.sendMessage(citel.chat, buttonMessaged, {
+            quoted: citel,
+        });
+
+    }
+      
+      
 
 
-    fetch(githubRepoUrl)
-        .then(response => response.json())
-        .then(data => {
-            const repoInfo = {
-                stars: data.stargazers_count,
-                forks: data.forks_count,
-                lastUpdate: data.updated_at,
-                owner: data.owner.login
-            };
-
-            // Format the date
             
-           
-
-            Void.sendPoll(citel.chat, uy, ['.menu', '.ping'], {
-                quoted: citel
-            });
-        })
-
-                footer: tlang().footer,
-                headerType: 4,
-            };
-            return Void.sendMessage(citel.chat, buttonMessage, {
-                quoted: citel,
-            });
-        }
-    )
