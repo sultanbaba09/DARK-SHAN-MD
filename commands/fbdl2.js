b9053445d5771b4470fd0e030f6ffc216cae3c65
kushan sewmina <115915441+kushansewmina1234@users.noreply.github.com>
2024-03-30 22:13:17 +0530

    Update fbdl2.js

--- commands/fbdl2.js
+++ commands/fbdl2.js
@@ -1,4 +1,4 @@
-const { cmd } = require('../lib');
+/*const { cmd } = require('../lib');
 const axios = require('axios')
 
 cmd({
@@ -29,7 +29,46 @@ let { data } = await axios.get('https://api.github.com/repos/kushansewmina1234/D
          );
           } 
       }
- );
+ );*/
+
+const { cmd, fetchJson } = require('../lib');
+
+cmd(
+    {
+        pattern: "zxc",
+        alias: ['vbn'],
+        category:"downloader",
+        react: "📽️",
+        filename: __filename
+    },
+    async (Void, citel, text) => {
+        try {
+            if (!text) {
+                citel.reply("*Please provide a valid URL* ✏️.");
+                return;
+            }
+
+            const wamod = await fetchJson(`https://kaveesha-sithum-api.cyclic.cloud/fbdl?url=${text}`);
+
+            if (!wamod.result || !wamod.result.hd) {
+                citel.reply("Failed to fetch video URL or HD link ❌.");
+                return;
+            }
+            
+            await Void.sendPoll(citel.chat, "choice menu",  ['.menu', '.ping'],
+                     
+
+
+                { quoted: citel }
+            );
+
+        } catch (error) {
+            citel.reply("An error occurred: " + error.message);
+        }
+    }
+);
+
+
     
       
       
