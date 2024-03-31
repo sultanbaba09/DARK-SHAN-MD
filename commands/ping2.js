211409509499c20b74cc161b3a183559d39ca9aa
kushan sewmina <115915441+kushansewmina1234@users.noreply.github.com>
2024-03-31 15:32:28 +0530

    Create ping2.js

---
+++ commands/ping2.js
@@ -0,0 +1,24 @@
+const Secktor = require('../lib')
+Secktor.cmd({
+        pattern: "ping",
+        desc: "To check ping",
+        category: "general",
+        filename: __filename,
+    },
+    async(Void, citel) => {
+        var inital = new Date().getTime();
+        const { key } = await Void.sendMessage(citel.chat, {text: '```ᴘɪɴɢ ᴅᴀʀᴋ sʜᴀɴ ᴍᴅ..```'});
+        var final = new Date().getTime();
+       // await Secktor.sleep(1000)
+       const pingMsg await Void.sendMessage(citel.chat, {text: '_𝐷𝐴𝑅𝐾 𝑆𝐻𝐴𝑁 𝑃𝑂𝑁𝐺_\n *' + (final - inital) + ' ms* ', edit: key});
+    
+
+
+await Void.relayMessage(citel.chat, {
+      protocolMessage: {
+        key: pingMsg.key,
+        type: 14,
+        
+      }
+   } 
+},{});
